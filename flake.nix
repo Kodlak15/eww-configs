defef06b102048521918cc715be1a369b1af0f60
Kodlak15 <stanlcod15@gmail.com>
2024-06-13 20:45:01 -0700

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "My Eww configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      mkDerivation = pkgs.stdenv.mkDerivation;
+    in {
+      packages = {
+        # Desktop eww config
+        everest = mkDerivation rec {
+          name = "eww";
+          src = ./everest;
+
+          buildCommand = ''
+            cp -r ${src} $out
+          '';
+        };
+        # Laptop eww config
+        denali = mkDerivation rec {
+          name = "eww";
+          src = ./denali;
+
+          buildCommand = ''
+            cp -r ${src} $out
+          '';
+        };
+      };
+    });
+}
