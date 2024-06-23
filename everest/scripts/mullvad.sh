85f22e0b4e5052dc8232d591f01d5ed6d41ae336
Kodlak15 <stanlcod15@gmail.com>
2024-06-23 16:02:45 -0700

    Fix mullvad listener

--- everest/scripts/mullvad.sh
+++ everest/scripts/mullvad.sh
@@ -5,33 +5,33 @@ launch_mullvad_applet() {
 }
 
 get_server_name() {
-	mullvad status | head -n 1 | awk -F ' ' '{print $3}'
+	mullvad status | head -n 1 | awk -F ' ' '{print $3}' 2>/dev/null
 }
 
 get_server_ip() {
-	mullvad status | tail -n 1 | awk -F 'IPv4: ' '{print $2}'
+	mullvad status | tail -n 1 | awk -F 'IPv4: ' '{print $2}' 2>/dev/null
 }
 
 get_host_ip() {
-	ifconfig | grep "inet" | awk 'NR==2' | xargs | awk -F ' ' '{print $2}'
+	ifconfig | grep "inet" | awk 'NR==2' | xargs | awk -F ' ' '{print $2}'  2>/dev/null
 }
 
 status() {
-	mullvad status | head -n 1 | awk -F ' ' '{print $1}'
+	mullvad status | head -n 1 | awk -F ' ' '{print $1}' 2>/dev/null
 }
 
 toggle() {
-	status="$(status)"
-	if [[ "$status" == "Connecting" ]]; then
-		status="Connected"
-	fi
-
-	case "$status" in
-		"Connected") mullvad disconnect;;
-		"Disconnected") mullvad connect;;
+	current_status="$(status)"
+	case "$current_status" in
+		"Connected") 
+			mullvad disconnect
+			echo "Disconnected" > "./listen/mullvad-state"
+			;;
+		"Disconnected") 
+			mullvad connect
+			echo "Connected" > "./listen/mullvad-state"
+			;;
 	esac
-
-	echo "$status" >> "./listen/mullvad-state"
 }
 
 case "$1" in 
