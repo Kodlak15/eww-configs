60d98836e989c48ba0662dd784309f9504b39ee7
Kodlak15 <stanlcod15@gmail.com>
2024-07-31 23:23:02 -0700

    Trying to build an app launcher

--- everest/scripts/applaunch.sh
+++ everest/scripts/applaunch.sh
@@ -16,7 +16,7 @@ steam() {
 	if [[ -n "$(command -v nvidia-offload)" ]]; then
 		exec nvidia-offload steam &
 	else
-	  exec steam &
+		exec steam &
 	fi
 }
 
@@ -41,7 +41,12 @@ wireshark() {
 	exec wireshark &
 }
 
-case "$1" in
+mullvad() {
+	exec mullvad-vpn &
+}
+
+run() {
+	case "$1" in
 	--librewolf) librewolf ;;
 	--firefox) firefox ;;
 	--spotify) spotify ;;
@@ -51,5 +56,27 @@ case "$1" in
 	--wireshark) wireshark ;;
 	--gimp) gimp ;;
 	--logseq) logseq ;;
-	*) echo "Invalid command..."
-esac 
+	--mullvad) mullvad ;;
+	*) echo "Invalid command..." ;;
+	esac
+}
+
+max_retries=3
+delay=2
+attempt=1
+
+# Make sure process is properly cleaned up when the application is closed
+while [ $attempt -le $max_retries ]; do
+	if run "$@"; then
+		break
+	else
+		echo "Command failed on attempt $attempt"
+		attempt=$((attempt + 1))
+		if [ $attempt -le $max_retries ]; then
+			echo "Retrying in $delay seconds..."
+			sleep $delay
+		else
+			echo "Reached maximum retries. Command failed."
+		fi
+	fi
+done
